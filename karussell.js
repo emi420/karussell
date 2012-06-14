6edca2d9c85a9a0ab37e69ad5deeeed8137cc5a0
Emilio Mariscal <emi420@gmail.com>
2012-06-14 10:48:17 -0300

    Bugfix (maxdist calc)

--- karussell.js
+++ karussell.js
@@ -1,31 +1,12 @@
 /**
  * Karussell, another jQuery carousel
  * 
+ * https://github.com/emi420/karussell
+ * 
  * Karussell is licensed under the terms of either the MIT License or the GNU General Public License (GPL) Version 3.
  * 
  * (c) 2012 Emilio Mariscal
- *
- * Usage:
- *
- *   <div id="carousel-wrapper" class="carousel-wrapper">    
- *     <a id="carousel-prev" class="carousel-prev" href="#prev">Prev</a>
- *     <a id="carousel-next" class="carousel-next" href="#next">Next</a> 
- *     <ul id="carousel" class="carousel">
- *         <li><img src="img/photo1.jpg" /></li>
- *         <li><img src="img/photo2.jpg" /></li>
- *         <li><img src="img/photo3.jpg" /></li> 
- *     </ul>
- *   </div>
- *
- *   $(document).ready(function () {
- *       $.karussell({
- *           id: "carousel",
- *           wrapper: "carousel-wrapper",
- *           prev: "carousel-prev",
- *           next: "carousel-next"
- *       });
- *   })
- *
+ * 
  */
 
 (function($){
@@ -41,6 +22,7 @@
         this.$items = {};
         this.width = this.$container[0].offsetWidth;
         this.$wrapper = $("#" + options.wrapper);
+        this.maxdist = 0;
         this.wrapper = {
             width: this.$wrapper[0].offsetWidth
         }
@@ -55,16 +37,16 @@
         init: function() {
             var fn = this;
             this.$items = this.$container.find("li");
-            this.$items.each(function(index, item) {
-               console.log(index); 
-            });            
             this.$prev.click(function(e) {
                 fn.left();
             });
             this.$next.click(function(e) {
                 fn.right();
             });
-            this.maxdist = this.width - this.wrapper.width;
+            this.$items.each(function(index, obj) {
+                fn.maxdist += obj.offsetWidth;
+            });
+
         },
 
         left: function() {
@@ -79,10 +61,11 @@
 
         right: function() {
             var sum;
+
             if ((sum = this.x - DISTANCE) > -this.maxdist) {
                 this.x = sum;
             } else {
-                this.x = -this.maxdist;          
+                this.x = -this.maxdist;       
             }
             this.$container.css("marginLeft",this.x + "px");
         },
@@ -95,5 +78,3 @@
     });
 
 }(jQuery));
-
-
